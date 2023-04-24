9b58516d6c9d8aaec5c7e8a9109678300d27b489
Lynn <lynn@jansheski.com>
2023-04-23 17:45:02 -0700

    deleting TODO

--- src/components/NewDrugInput.js
+++ src/components/NewDrugInput.js
@@ -28,7 +28,6 @@ const NewDrugInput = (props) => {
     }
   }, [props.drugData]);
 
-  // TODO: Add dose ranges to drug table to alert user when out of range
   const onDrugChange = (drug) => {
     setSelectedDrug(drug);
 
